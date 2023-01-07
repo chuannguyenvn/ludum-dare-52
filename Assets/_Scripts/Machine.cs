3396f9774b8c548ffc56ee498fb2846da3ff2a79
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-01-07 14:48:29 +0700

    feat: shrink saw after a while

--- Assets/_Scripts/Machine.cs
+++ Assets/_Scripts/Machine.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Machine : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private float effectiveDuration = 5f;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(effectiveDuration);
+        yield return Shrink();
+    }
+
+    private IEnumerator Shrink()
+    {
+        while (transform.localScale.x > 0.01f)
+        {
+            transform.localScale -= Vector3.one * Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
 
     public void SetVelocity(Vector2 velocity)
     {
